8355613d2fe0b9a3264595f755a945220fb95c08
Ben Schoeffmann <benedikt.schoeffmann@medani.com>
2023-05-09 16:35:35 +0200

    Change: update generation script

--- openapi/generate.sh
+++ openapi/generate.sh
@@ -2,6 +2,12 @@
 DEST_DIR="../lib/RtgConnectBundle";
 SPEC_FILE="openapi.json"
 CONFIG_FILE="config.json"
+SPEC_ONLINE="https://rtg-portal.picard.de/api/shop/v2/openapi.json"
+DATE=$(date +%F_%X);
+
+curl $SPEC_ONLINE -o openapi.$DATE.json;
+rm openapi.json
+ln -s openapi.$DATE.json openapi.json
 
 rm -rf $DEST_DIR;
 openapi-generator-cli generate -g php -i $SPEC_FILE -o $DEST_DIR -c $CONFIG_FILE
