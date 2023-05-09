19849b7ded512e4eabb98eddbaa0f26afb8a4297
Ben Schoeffmann <benedikt.schoeffmann@medani.com>
2023-05-09 16:11:58 +0200

    Add: automatic generator scripts, symfony/dumper component

--- openapi/generate.sh
+++ openapi/generate.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-openapi-generator-cli generate -g php -i openapi.json -o ../lib -c config.json
+DEST_DIR="../lib/RtgConnectBundle";
+SPEC_FILE="openapi.json"
+CONFIG_FILE="config.json"
+
+rm -rf $DEST_DIR;
+openapi-generator-cli generate -g php -i $SPEC_FILE -o $DEST_DIR -c $CONFIG_FILE
 
