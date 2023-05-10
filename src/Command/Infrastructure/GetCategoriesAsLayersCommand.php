b1d9507b40309ff4759db037e892e74bc73ccb7d
Ben Schoeffmann <benedikt.schoeffmann@medani.com>
2023-05-10 14:20:24 +0200

    Add: get Categories as Layers Command

---
+++ src/Command/Infrastructure/GetCategoriesAsLayersCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Command\Infrastructure;
+
+use App\Api;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'test:get-categories-as-layers',
+    description: 'Gets a list of all Categories',
+)]
+class GetCategoriesAsLayersCommand extends Command
+{
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $api = new Api();
+        $infrastructuralApi = $api->getInfrastructuralApi();
+
+        $response = $infrastructuralApi->getProductCategories($api->getStocklistToken());
+
+        $io->title("Get Categories As Layers");
+
+        $layers = [];
+        
+        $categories = $response->getData();
+
+        foreach ($categories as $category) {
+            $layers[$category->getLayer()][$category->getId()] = $category;
+        }
+        
+        dump($layers);
+
+        return Command::SUCCESS;
+    }
+}
